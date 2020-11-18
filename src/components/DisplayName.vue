33c64a63838493978ebb498c9bc490fa72ca06fb
Ben Huang <benxu.huang@gmail.com>
2020-11-18 11:24:08 +0800

    first commit

---
+++ src/components/DisplayName.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h2>{{name2}}</h2>
+  </div>
+</template>
+
+<script>
+import { mapState} from 'vuex'
+export default {
+    data() {
+        return {
+            name2: name
+        };
+    },
+    computed:{
+        ...mapState([name])
+    }
+}
+</script>
